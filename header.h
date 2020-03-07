c19450449eb2fdefb35a2af9db0550b5a78eea8f
Tejasmallikarjuncj <tejasteju147malli@gmail.com>
2020-03-07 17:03:57 +0530

    i have defined the struct in header.h

---
+++ header.h
@@ -0,0 +1,17 @@
+typedef struct client {
+int pin;
+long long int account_no;
+char* name;
+char* acount_type;
+float balance;
+char* bank_name;
+long long int card;
+} client;
+
+client[]* load(void);
+client* search(client []);
+
+
+
+
+
