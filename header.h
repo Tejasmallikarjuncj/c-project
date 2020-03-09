84be07bb49d9687382f329ad1e3cde953652eaf9
Tejasmallikarjuncj <tejasteju147malli@gmail.com>
2020-03-09 18:22:14 +0530

    added a struct for gui

--- header.h
+++ header.h
@@ -6,9 +6,20 @@ char* acount_type;
 float balance;
 char* bank_name;
 long long int card;
-} client;
+}client;
+
+typedef struct gui {
+GtkWidget *win;
+GtkWidget *box;
+Gtkwidget *label[];
+GtkWidget *button[];
+GtkWidget *entry;
+GtkWidget *radio_button[];
+GtkWidget *dailog_box;
+}gui;
 
 client[]* load(void);
+
 client* search(client []);
 
 
