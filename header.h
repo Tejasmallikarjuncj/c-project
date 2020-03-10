39c5cdd374136bc854f0f99854702fdae4890945
Tejasmallikarjuncj <tejasteju147malli@gmail.com>
2020-03-10 10:29:38 +0530

    add header.h

--- header.h
+++ header.h
@@ -1,26 +1,27 @@
 typedef struct client {
-int pin;
-long long int account_no;
-char* name;
-char* acount_type;
+long int pin;
+long long int acc_no;
+char name[15];
+char acc_type[15];
 float balance;
-char* bank_name;
+char bank_name[15];
 long long int card;
-}client;
+}atm;
 
 typedef struct gui {
 GtkWidget *win;
 GtkWidget *box;
-Gtkwidget *label[];
-GtkWidget *button[];
+Gtkwidget *label[10];
+GtkWidget *button[3];
 GtkWidget *entry;
-GtkWidget *radio_button[];
-GtkWidget *dailog_box;
+GtkWidget *radio_button[4];
+GtkWidget *dailog_box[4];
 }gui;
 
-client[]* load(void);
+atm* load(void);
 
-client* search(client []);
+atm* search(atm *);
+
 
 
 
