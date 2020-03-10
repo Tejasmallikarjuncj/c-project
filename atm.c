9bfe64357270cbcafd9af21fafe8ed438eeba31c
Tejasmallikarjuncj <tejasteju147malli@gmail.com>
2020-03-10 19:39:32 +0530

    added all the function required

--- atm.c
+++ atm.c
@@ -1,3 +1,4 @@
+#include "header.h"
 #include <gtk/gtk.h>
 #include <gtk/gtkx.h>
 #include <stdlib.h>
@@ -5,140 +6,147 @@
 #include <signal.h>
 #include <sys/types.h>
 
-static int argc;
-char *argv[];
-GtkWidget *win; 
-GtkWidget *win2;
-GtkWidget *win3_a;
-GtkWidget *win3_b;
-GtkWidget *win3_c;
-GtkWidget *win3_d;
+atm client_pr;
 
-
-
-void win2_1 (int argc, char *argv[])
+void win2 (int argc, char *argv[])
 {
+gui pg2;
 gtk_init (&argc, &argv);
 GtkBuilder *builder;
 builder = gtk_builder_new_from_file ("layout1.glade");
-win2 = GTK_WIDGET(gtk_builder_get_object (builder,"win2"));
-g_signal_connect(win2, "destroy",G_CALLBACK(gtk_main_quit),NULL);
+pg2.win = GTK_WIDGET(gtk_builder_get_object (builder,"win2"));
+g_signal_connect(pg2.win,"destroy",G_CALLBACK(gtk_main_quit),NULL);
 gtk_builder_connect_signals(builder, NULL);
-GtkWidget *box2 = GTK_WIDGET(gtk_builder_get_object (builder,"box2"));
-GtkWidget *top = GTK_WIDGET(gtk_builder_get_object (builder,"top_lb_2"));
-GtkWidget *r1 = GTK_WIDGET(gtk_builder_get_object (builder,"r1"));
-GtkWidget *r2 = GTK_WIDGET(gtk_builder_get_object (builder,"r2"));
-GtkWidget *r3 = GTK_WIDGET(gtk_builder_get_object (builder,"r3"));
-GtkWidget *r4 = GTK_WIDGET(gtk_builder_get_object (builder,"r4"));
-GtkWidget *ext = GTK_WIDGET(gtk_builder_get_object (builder,"ext_1"));
-GtkWidget *sub = GTK_WIDGET(gtk_builder_get_object (builder,"submit_2"));
-gtk_widget_show(win2);
-gtk_main ();
-}
-
-void next()
-{
-gtk_main_quit();
-win2_1(argc,argv);
-}
-
-
-void win3_a_1 (int argc, char *argv[])
-{
-gtk_init (&argc, &argv);
-GtkBuilder *builder;
-builder = gtk_builder_new_from_file ("layout1.glade");
-win3_a = GTK_WIDGET(gtk_builder_get_object (builder,"win3_a"));
-g_signal_connect(win3_a, "destroy",G_CALLBACK(gtk_main_quit),NULL);
-gtk_builder_connect_signals(builder, NULL);
-GtkWidget *box3_a = GTK_WIDGET(gtk_builder_get_object (builder,"box3_a"));
-GtkWidget *top = GTK_WIDGET(gtk_builder_get_object (builder,"top_lb_3_a"));
-GtkWidget *name_1 = GTK_WIDGET(gtk_builder_get_object (builder,"name_1"));
-GtkWidget *bal_1 = GTK_WIDGET(gtk_builder_get_object (builder,"bal_1"));
-GtkWidget *Account_2 = GTK_WIDGET(gtk_builder_get_object (builder,"Account_2"));
-GtkWidget *name  = GTK_WIDGET(gtk_builder_get_object (builder,"account"));
-GtkWidget *bal = GTK_WIDGET(gtk_builder_get_object (builder,"bal"));
-GtkWidget *Prev_1 = GTK_WIDGET(gtk_builder_get_object (builder,"Prev_1"));
-GtkWidget *ext_2 = GTK_WIDGET(gtk_builder_get_object (builder,"ext_2"));
-gtk_widget_show(win3_a);
-gtk_main ();
-}
-
-void win3_b_1 (int argc, char *argv[])
-{
-gtk_init (&argc, &argv);
-GtkBuilder *builder;
-builder = gtk_builder_new_from_file ("layout1.glade");
-win3_b = GTK_WIDGET(gtk_builder_get_object (builder,"win3_b"));
-g_signal_connect(win3_b, "destroy",G_CALLBACK(next),NULL);
-gtk_builder_connect_signals(builder, NULL);
-GtkWidget *box3_b = GTK_WIDGET(gtk_builder_get_object (builder,"box3_b"));
-GtkWidget *top = GTK_WIDGET(gtk_builder_get_object (builder,"top_lb_3_b"));
-GtkWidget *entry_money = GTK_WIDGET(gtk_builder_get_object (builder,"entry_money"));
-GtkWidget *money_entry = GTK_WIDGET(gtk_builder_get_object (builder,"money_entry"));
-GtkWidget *submit_3_a= GTK_WIDGET(gtk_builder_get_object (builder,"submit_3_a"));
-gtk_widget_show(win3_b);
+pg2.box = GTK_WIDGET(gtk_builder_get_object (builder,"box2"));
+pg2.label[0] = GTK_WIDGET(gtk_builder_get_object (builder,"top_lb_2"));
+pg2.radio_button[0] = GTK_WIDGET(gtk_builder_get_object (builder,"r1"));
+g_signal_connect(pg2.radio_button[0],"toggled",G_CALLBACK(next1),NULL);
+pg2.radio_button[1] = GTK_WIDGET(gtk_builder_get_object (builder,"r2"));
+g_signal_connect(pg2.radio_button[1],"toggled",G_CALLBACK(next2),NULL);
+pg2.radio_button[2] = GTK_WIDGET(gtk_builder_get_object (builder,"r3"));
+g_signal_connect(pg2.radio_button[2],"toggled",G_CALLBACK(next3),NULL);
+pg2.radio_button[3]= GTK_WIDGET(gtk_builder_get_object (builder,"r4"));
+g_signal_connect(pg2.radio_button[3],"toggled",G_CALLBACK(next4),NULL)
+pg2.button[0] = GTK_WIDGET(gtk_builder_get_object (builder,"ext_1"));
+pg2.button[2] = GTK_WIDGET(gtk_builder_get_object (builder,"submit_2"));
+gtk_widget_show(pg2.win);
 gtk_main ();
 }
 
 
-void win3_c_1 (int argc, char *argv[])
+
+
+void win3_a (int argc, char *argv[], atm *)
 {
+gui pg3a;
 gtk_init (&argc, &argv);
 GtkBuilder *builder;
 builder = gtk_builder_new_from_file ("layout1.glade");
-win3_c = GTK_WIDGET(gtk_builder_get_object (builder,"win3_c"));
-g_signal_connect(win3_c, "destroy",G_CALLBACK(gtk_main_quit),NULL);
+pg3a.win = GTK_WIDGET(gtk_builder_get_object (builder,"win3_a"));
+g_signal_connect(pg3a.win, "destroy",G_CALLBACK(gtk_main_quit),NULL);
 gtk_builder_connect_signals(builder, NULL);
-GtkWidget *box3_c = GTK_WIDGET(gtk_builder_get_object (builder,"box3_c"));
-GtkWidget *top = GTK_WIDGET(gtk_builder_get_object (builder,"top_lb_3_c"));
-GtkWidget *new_pin = GTK_WIDGET(gtk_builder_get_object (builder,"new_pin"));
-GtkWidget *entry_new_pin = GTK_WIDGET(gtk_builder_get_object (builder,"entry_new_pin"));
-GtkWidget *confirm_pin = GTK_WIDGET(gtk_builder_get_object (builder,"confirm_pin"));
-GtkWidget *entry_confirm_pin = GTK_WIDGET(gtk_builder_get_object (builder,"entry_confirm_pin"));
-GtkWidget *submit_3_c = GTK_WIDGET(gtk_builder_get_object (builder,"submit_3_c"));
-gtk_widget_show(win3_c);
+pg3a.box = GTK_WIDGET(gtk_builder_get_object (builder,"box3_a"));
+pg3a.label[0] = GTK_WIDGET(gtk_builder_get_object (builder,"top_lb_3_a"));
+pg3a.label[1] = GTK_WIDGET(gtk_builder_get_object (builder,"name_1"));
+pg3a.label[2] = GTK_WIDGET(gtk_builder_get_object (builder,"bal_1"));
+pg3a.label[3] = GTK_WIDGET(gtk_builder_get_object (builder,"Account_2"));
+pg3a.label[4] = GTK_WIDGET(gtk_builder_get_object (builder,"name"));
+gtk_label_set_text(GTK_LABEL(pg3a.label[4]), client_pr.name);
+pg3a.label[5]  = GTK_WIDGET(gtk_builder_get_object (builder,"account no"));
+gtk_label_set_text(GTK_LABEL(pg3a.label[5]), client_pr.acc_no);
+pg3a.label[6] = GTK_WIDGET(gtk_builder_get_object (builder,"bal"));
+gtk_label_set_text(GTK_LABEL(pg3a.label[6]), client_pr.balance);
+pg3a.box[0] = GTK_WIDGET(gtk_builder_get_object (builder,"Prev_1"));
+g_signal_connect(pg3a.box[0],"clicked",G_CALLBACK(prev),NULL);
+pg3a.box[1] = GTK_WIDGET(gtk_builder_get_object (builder,"ext_2"));
+g_signal_connect(pg3a.box[1], "clicked",G_CALLBACK(gtk_main_quit),NULL);
+gtk_widget_show(pg3a.win);
 gtk_main ();
 }
 
-void win3_d_1 (int argc, char *argv[])
+void win3_b (int argc, char *argv[])
 {
+gui pg3b;
 gtk_init (&argc, &argv);
 GtkBuilder *builder;
 builder = gtk_builder_new_from_file ("layout1.glade");
-win3_d = GTK_WIDGET(gtk_builder_get_object (builder,"win3_d"));
-g_signal_connect(win3_d, "destroy",G_CALLBACK(gtk_main_quit),NULL);
+pg3b.win = GTK_WIDGET(gtk_builder_get_object (builder,"win3_b"));
+g_signal_connect(pg3b.win, "destroy",G_CALLBACK(next),NULL);
 gtk_builder_connect_signals(builder, NULL);
-GtkWidget *box3_d = GTK_WIDGET(gtk_builder_get_object (builder,"box3_d"));
-GtkWidget *top = GTK_WIDGET(gtk_builder_get_object (builder,"top_lb_3_d"));
-GtkWidget *entry_money_d = GTK_WIDGET(gtk_builder_get_object (builder,"entry_money_d"));
-GtkWidget *entry_money_c = GTK_WIDGET(gtk_builder_get_object (builder,"entry_money_c"));
-GtkWidget *submit_3_d = GTK_WIDGET(gtk_builder_get_object (builder,"submit_3_d"));
-gtk_widget_show(win3_d);
+pg3b.box[0] = GTK_WIDGET(gtk_builder_get_object (builder,"box3_b"));
+pg3b.label[0] = GTK_WIDGET(gtk_builder_get_object (builder,"top_lb_3_b"));
+pg3b.label[1] = GTK_WIDGET(gtk_builder_get_object (builder,"entry_money"));
+pg3b.entry[0] = GTK_WIDGET(gtk_builder_get_object (builder,"money_entry"));
+pg3b.button[0] = GTK_WIDGET(gtk_builder_get_object (builder,"submit_3_a"));
+g_signal_connect(pg3.button[0], "clicked",G_CALLBACK(submit3a),pg3b.entry);
+gtk_widget_show(pg3b.win);
 gtk_main ();
 }
 
-void win1_1 (int argc, char *argv[])
+
+void win3_c (int argc, char *argv[])
 {
+gui pg3c;
 gtk_init (&argc, &argv);
 GtkBuilder *builder;
 builder = gtk_builder_new_from_file ("layout1.glade");
-win = GTK_WIDGET(gtk_builder_get_object (builder,"win1"));
-g_signal_connect(win, "destroy",G_CALLBACK(next),NULL);
+pg3c.win = GTK_WIDGET(gtk_builder_get_object (builder,"win3_c"));
+g_signal_connect(pg3c.win, "destroy",G_CALLBACK(gtk_main_quit),NULL);
 gtk_builder_connect_signals(builder, NULL);
-GtkWidget *gid1 = GTK_WIDGET(gtk_builder_get_object (builder,"box1"));
-GtkWidget *top = GTK_WIDGET(gtk_builder_get_object (builder,"top_lb_1"));
-GtkWidget *pin = GTK_WIDGET(gtk_builder_get_object (builder,"pin_lb"));
-GtkWidget *entry = GTK_WIDGET(gtk_builder_get_object (builder,"entry"));
-GtkWidget *sub = GTK_WIDGET(gtk_builder_get_object (builder,"submit_1"));
-gtk_widget_show(win);
+pg3c.box = GTK_WIDGET(gtk_builder_get_object (builder,"box3_c"));
+pg3c.label[0] = GTK_WIDGET(gtk_builder_get_object (builder,"top_lb_3_c"));
+pg3c.label[1] = GTK_WIDGET(gtk_builder_get_object (builder,"new_pin"));
+pg3c.entry[0] = GTK_WIDGET(gtk_builder_get_object (builder,"entry_new_pin"));
+pg3c.label[2] = GTK_WIDGET(gtk_builder_get_object (builder,"confirm_pin"));
+pg3c.entry[1] = GTK_WIDGET(gtk_builder_get_object (builder,"entry_confirm_pin"));
+pg3c.button[0] = GTK_WIDGET(gtk_builder_get_object (builder,"submit_3_c"));
+g_signal_connect(pg3c.button[0],"clicked",G_CALLBACK(submit3c),pg3c.entry[0]);
+gtk_widget_show(pg3c);
+gtk_main ();
+}
+
+void win3_d (int argc, char *argv[])
+{
+gui pg3d;
+gtk_init (&argc, &argv);
+GtkBuilder *builder;
+builder = gtk_builder_new_from_file ("layout1.glade");
+pg3d.win = GTK_WIDGET(gtk_builder_get_object (builder,"win3_d"));
+g_signal_connect(pg3d.win, "destroy",G_CALLBACK(gtk_main_quit),NULL);
+gtk_builder_connect_signals(builder, NULL);
+pg3d.box = GTK_WIDGET(gtk_builder_get_object (builder,"box3_d"));
+pg3d.label[0] = GTK_WIDGET(gtk_builder_get_object (builder,"top_lb_3_d"));
+pg3d.label[1] = GTK_WIDGET(gtk_builder_get_object (builder,"entry_money_d"));
+pg3d.entry[0] = GTK_WIDGET(gtk_builder_get_object (builder,"entry_money_c"));
+pg3d.button[0] = GTK_WIDGET(gtk_builder_get_object (builder,"submit_3_d"));
+g_signal_connect(pg3d.button[0],"clicked",G_CALLBACK(submit3d),pg3d.entry[0]);
+gtk_widget_show(pg3d.win);
+gtk_main ();
+}
+
+void win1(int argc, char *argv[])
+{
+gui pg1
+gtk_init (&argc, &argv);
+GtkBuilder *builder;
+builder = gtk_builder_new_from_file ("layout1.glade");
+pg1.win = GTK_WIDGET(gtk_builder_get_object (builder,"win1"));
+g_signal_connect(pg1.win, "destroy",G_CALLBACK(next),NULL);
+gtk_builder_connect_signals(builder, NULL);
+pg1.box = GTK_WIDGET(gtk_builder_get_object (builder,"box1"));
+pg1.label[0] = GTK_WIDGET(gtk_builder_get_object (builder,"top_lb_1"));
+pg1.label[1] = GTK_WIDGET(gtk_builder_get_object (builder,"pin_lb"));
+pg1.entry[0] = GTK_WIDGET(gtk_builder_get_object (builder,"entry"));
+pg1.button[0] = GTK_WIDGET(gtk_builder_get_object (builder,"submit_1"));
+g_signal_connect(pg1.button[0],"clicked",G_CALLBACK(next1),pg1.entry[0]);
+gtk_widget_show(pg1.win);
 gtk_main ();
 }
 
 int main(int argc, char *argv[])
 {
-win1_1(argc, argv);
-//win2(argc, argv);
+//win1_1(argc, argv);
+win2(argc, argv);
 //win3_a(argc, argv);
 //win3_b(argc, argv);
 //win3_c(argc, argv);
--- header.h
+++ header.h
@@ -1,26 +1,63 @@
+#include <gtk/gtk.h>
+
 typedef struct client {
-long int pin;
+int pin;
 long long int acc_no;
 char name[15];
 char acc_type[15];
 float balance;
 char bank_name[15];
 long long int card;
+int retrn; 
 }atm;
 
 typedef struct gui {
 GtkWidget *win;
 GtkWidget *box;
-Gtkwidget *label[10];
+GtkWidget *label[10];
 GtkWidget *button[3];
 GtkWidget *entry;
 GtkWidget *radio_button[4];
 GtkWidget *dailog_box[4];
 }gui;
 
+void win1(int, char* []);
+
+void win2(int, char* []);
+
+void win3_a(int, char* []);
+
+void win3_b(int, char* []);
+
+void win3_c(int, char* []);
+
+void win3_d(int, char* []);
+
+void next1(GtkWiget *,gpointer);
+
+void next2(GtkWiget *,gpointer);
+
+void next3(GtkWiget *,gpointer);
+
+void next4(GtkWiget *,gpointer);
+
+void prev(GtkWiget *,gpointer);
+
+void submit3a(GtkWiget *,gpointer);
+
+void submit3c(GtkWiget *,gpointer);
+
+void submit3d(GtkWiget *,gpointer);
+
 atm* load(void);
 
 atm* search(atm *);
+
+void wrt_bal(long long int);
+
+char* typ_int(int);
+
+char* typ_flt(float);
 
 
 
